4aca62275215362805cc265e85e938e8ae79cec0
ahmedmedhat1994 <112586537+ahmedmedhat1994@users.noreply.github.com>
2023-03-19 15:32:52 +0200

    02

--- filewebhook/hookgroup.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-header('content-type: application/json');
-$data = json_decode(file_get_contents('php://input'), true);
-file_put_contents('hookgroup.txt', '[' . date('Y-m-d H:i:s') . "]\n" . json_encode($data) . "\n\n", FILE_APPEND);
-$nomor = $data['number']; // nomor pengguna
-$idgroup = $data['groupid']; // id groupnya
-$aksi = $data['action']; // aksinya, masuk = add ,keluar / di kick = remove
--- index.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once('helper/koneksi.php');
-header('Location:'.$base_url.'auth/login.php');
